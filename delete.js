8f419ad761943ad323c5515cf6581deeffee419e
Vania Dimitrova <vani4ka66@abv.bg>
2017-07-22 09:43:07 +0300

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,7 +1,7 @@
 
 define([], function deleted() {
     return (array, [index]) => {
-            array.splice(index, 1);
+            array.splice(Number(index), 1);
         return array;
     }
 });
