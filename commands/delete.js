8b750e8c701e9a2925a45bfec4c5ec52e553941a
Vania Dimitrova <vani4ka66@abv.bg>
2017-07-23 01:25:31 +0300

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -1,8 +1,12 @@
 define([], function deleteItem() {
     return (array, [index]) => {
-        if (index < 0 || index >= array.length || Number.isNaN(+index)) {
+        if (index < 0 || index >= array.length) {
             throw new Error(`Invalid index.`);
         }
+	else if (Number.isNaN(+index)) {
+            throw new Error(`Invalid command parameters.`);
+        }
+
         array.splice(Number(index), 1);
         return array;
     }
