a3a389e79be85a2d68a157dda363bd2c37f03e93
Vania Dimitrova <vani4ka66@abv.bg>
2017-07-22 08:48:35 +0300

    Update append.js

--- append.js
+++ append.js
@@ -1,6 +1,6 @@
 define([], function append(value) {
     return (arr) => {
-        if(typeof value === string || value !== '' ) {
+        if(typeof value === string && value !== '' ) {
             arr.push(value);
         }
         return arr;
