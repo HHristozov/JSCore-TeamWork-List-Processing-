8365f48e1c470e8381e2a619272929e22e0bc2bc
Vania Dimitrova <vani4ka66@abv.bg>
2017-07-22 23:52:40 +0300

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -1,7 +1,7 @@
 define([], function deleteItem() {
     return (array, [index]) => {
-        if (index < 0 || index >= array.length) {
-            throw new Error("Out of range index.");
+        if (index < 0 || index >= array.length || Number.isNaN(+index)) {
+            throw new Error(`Invalid index.`);
         }
         array.splice(Number(index), 1);
         return array;
